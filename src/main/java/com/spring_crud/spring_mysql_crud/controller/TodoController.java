0a6f6b307e06c5599dbb9d9a41b9377dec5fa0de
kusham <kusham1998@gmail.com>
2022-10-22 22:44:16 +0530

    add controller

--- src/main/java/com/spring_crud/spring_mysql_crud/controller/TodoController.java
+++ src/main/java/com/spring_crud/spring_mysql_crud/controller/TodoController.java
@@ -3,14 +3,30 @@ package com.spring_crud.spring_mysql_crud.controller;
 import com.spring_crud.spring_mysql_crud.entity.Product;
 import com.spring_crud.spring_mysql_crud.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
 @RestController
-public class ProductController {
+public class TodoController {
     @Autowired
     private ProductService service;
+
+    @RequestMapping("/todo-list")
+    public ModelAndView showTodoList() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("todo-list.html");
+        return modelAndView;
+    }
+
+    @RequestMapping("/todo-update")
+    public ModelAndView updateTodoList() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("todo-list.html");
+        return modelAndView;
+    }
 
     @PostMapping("/product")
     public void add(@RequestBody Product product){

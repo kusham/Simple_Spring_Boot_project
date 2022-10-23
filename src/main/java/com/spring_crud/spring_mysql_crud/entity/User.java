0d94200fc744fc370e4ca3ca8de69164be9b37e9
kusham <kusham1998@gmail.com>
2022-10-23 23:25:09 +0530

    add user class

---
+++ src/main/java/com/spring_crud/spring_mysql_crud/entity/User.java
@@ -0,0 +1,23 @@
+package com.spring_crud.spring_mysql_crud.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "USERS")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "email", nullable = false, unique = true, length = 45)
+    private String email;
+
+    @Column(name = "password", nullable = false, length = 64)
+    private String password;
+
+    @Column(name = "first_name", nullable = false, length = 20)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 20)
+    private String lastName;
+}

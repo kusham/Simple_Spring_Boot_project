a7896200057928e2c897717faf4c4a47da5252b4
kusham <kusham1998@gmail.com>
2022-10-24 01:55:15 +0530

    changes for AuthService

--- src/main/java/com/spring_crud/spring_mysql_crud/service/AuthService.java
+++ src/main/java/com/spring_crud/spring_mysql_crud/service/AuthService.java
@@ -1,6 +1,8 @@
 package com.spring_crud.spring_mysql_crud.service;
 
 
+import com.spring_crud.spring_mysql_crud.entity.Product;
+import com.spring_crud.spring_mysql_crud.entity.User;
 import com.spring_crud.spring_mysql_crud.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,4 +14,8 @@ import javax.transaction.Transactional;
 public class AuthService {
     @Autowired
     private UserRepository userRepository;
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
 }

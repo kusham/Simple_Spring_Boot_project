1c28d5240cdf238a125df12bb66d7b8dd8306f2d
kusham <kusham1998@gmail.com>
2022-10-24 23:59:57 +0530

    minor changes

--- src/main/java/com/spring_crud/spring_mysql_crud/controller/AuthController.java
+++ src/main/java/com/spring_crud/spring_mysql_crud/controller/AuthController.java
@@ -4,6 +4,9 @@ import com.spring_crud.spring_mysql_crud.entity.User;
 import com.spring_crud.spring_mysql_crud.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -12,23 +15,35 @@ public class AuthController {
     @Autowired
     private AuthService authService;
 
-    @RequestMapping("/login")
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
     public ModelAndView viewLogin() {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("login.html");
         return modelAndView;
     }
 
-    @RequestMapping("/register")
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
     public ModelAndView viewRegister() {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("register.html");
         return modelAndView;
     }
 
-    @RequestMapping(value = "/userRegister", method = RequestMethod.POST)
-    public String userRegister(@ModelAttribute("user") User user) {
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public String userRegister(@RequestBody User user, BindingResult bindingResult, ModelMap modelMap){
+        ModelAndView modelAndView = new ModelAndView();
+        System.out.println(user);
         authService.save(user);
-        return "login.html";
+//        if(bindingResult.hasErrors()){
+//            modelAndView.addObject("successMessage", "Please add correct details!");
+//            modelMap.addAttribute("bindingResult", bindingResult);
+//        }else if(au.isUserPresent(user)){
+//            modelAndView.addObject("successMessage", "User already exists!");
+//        }else {
+//            userService.saveUser(user);
+//            modelAndView.addObject("successMessage", "User registered successfully!");
+//        }
+        return "User has registered";
+
     }
 }
